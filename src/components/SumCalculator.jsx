c41682f0e94a3fd13b93a186d4247937ddc7267c
Le Tan Hiep <tanhiep24135@gmail.com>
2025-11-05 16:15:25 +0700

    Final

--- src/components/SumCalculator.jsx
+++ src/components/SumCalculator.jsx
@@ -349,7 +349,7 @@ function SumCalculator() {
                 value={num1}
                 onChange={handleNum1Change}
                 placeholder="Nhập số thứ nhất (ví dụ: 123 hoặc -123)"
-                maxLength={21} // Giới hạn độ dài (tùy chọn)
+                maxLength={21}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm 
                 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 
                 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 
